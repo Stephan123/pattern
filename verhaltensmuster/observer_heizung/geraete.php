57c4741aa2db1ff731de7a9f6a8ef7d75a33c84e
Stephan Krauss <info@suppenterrine.de>
2013-07-12 09:28:22 +0200

    Observer Pattern Test am beispiel haushaltgeräte

---
+++ verhaltensmuster/observer_heizung/geraete.php
@@ -0,0 +1,14 @@
+<?php 
+ /**
+ * Beschreibung des Interface
+ *
+ * @author Stephan.Krauss
+ * @date 06.09.13
+ * @file geraete.php
+ * @package front | admin
+ * @subpackage interface
+ */
+ 
+interface geraete {
+
+}
---
+++ verhaltensmuster/observer_heizung/kuehlung.php
@@ -0,0 +1,19 @@
+<?php 
+ /**
+ * Beschreibung der Klasse
+ *
+ * + Ausführliche Beschreibung der Klasse
+ * + Ausführliche Beschreibung der Klasse
+ * + Ausführliche Beschreibung der Klasse
+ * 
+ * @author Stephan.Krauss
+ * @date 06.09.13
+ * @file kuehlung.php
+ * @package front | admin | tools | plugins | schnittstelle | tabelle
+ * @subpackage controller | model | interface | shadow | data
+ */
+ 
+class kuehlung
+{
+
+}
